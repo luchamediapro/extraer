4b9c42a01f2f4dcc85dbf9f320f0ca0153be1ea3
luchamediapro <entretenimientogaran@gmail.com>
2026-03-10 17:35:44 -0600

    Update node.js

--- node.js
+++ node.js
@@ -1,41 +1,11 @@
-const https = require("https");
-
-const url = "https://deportes.ksdjugfsddeports.com/tvporinternet3.php?stream=5_";
-
-function decodeBase64Multiple(str, times){
-    let result = str;
-    for(let i=0;i<times;i++){
-        result = Buffer.from(result, 'base64').toString('utf8');
-    }
-    return result;
+{
+  "name": "m3u8-extractor-render",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
 }
-
-https.get(url,(res)=>{
-
-    let data = "";
-
-    res.on("data",chunk=>{
-        data += chunk;
-    });
-
-    res.on("end",()=>{
-
-        const match = data.match(/atob\(atob\(atob\(atob\("([^"]+)"/);
-
-        if(!match){
-            console.log("No se encontró la URL codificada");
-            return;
-        }
-
-        const encoded = match[1];
-
-        const m3u8 = decodeBase64Multiple(encoded,4);
-
-        console.log("URL M3U8:");
-        console.log(m3u8);
-
-    });
-
-}).on("error",(err)=>{
-    console.log(err);
-});
